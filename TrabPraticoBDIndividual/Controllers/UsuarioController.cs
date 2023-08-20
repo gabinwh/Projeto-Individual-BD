65e69f2cd3ed4b4760908fe5b352c19bab320e5b
Gabriel <gcsprog@gmail.com>
2023-08-20 11:59:57 -0300

    feat: remove validacao de CPF

--- TrabPraticoBDIndividual/Controllers/UsuarioController.cs
+++ TrabPraticoBDIndividual/Controllers/UsuarioController.cs
@@ -19,7 +19,7 @@ public class UsuarioController : ControllerBase
         _mapper = mapper;
     }
     [HttpGet]
-    public async Task<IActionResult> Get()
+    public async Task<IActionResult> GetAll()
     {
         try
         {
@@ -27,6 +27,20 @@ public class UsuarioController : ControllerBase
 
             return Ok(usuarios);
         }catch (Exception ex) { return BadRequest(ex.Message); }
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        try
+        {
+            Usuario? usuario = await _dataContext.Usuarios.FindAsync(id);
+
+            if(usuario == null) throw new NullReferenceException("A entidade não existe");
+
+            return Ok(usuario);
+        }
+        catch (Exception ex) { return BadRequest(ex.Message); }
     }
 
     [HttpPost]
@@ -38,14 +52,6 @@ public class UsuarioController : ControllerBase
             var validator = new UsuarioValidation();
             var results = validator.Validate(request);
             if (!results.IsValid) throw new Exception(results.ToString());
-            if(request.cpf.ToString().Length == 10)
-            {
-                string cpfFormatado = "0" + request.cpf.ToString();
-                if (!ValidarCPF(cpfFormatado)) throw new Exception("CPF inválido");
-            }else if (request.cpf.ToString().Length == 11) 
-            {
-                if (!ValidarCPF(request.cpf.ToString())) throw new Exception("CPF inválido");
-            }
 
             Usuario usuario = _mapper.Map<UsuarioStoreRequest, Usuario>(request);
 
@@ -59,42 +65,5 @@ public class UsuarioController : ControllerBase
 
             return BadRequest(ex.Message);
         }
-    }
-
-    static bool ValidarCPF(string cpf)
-    {
-        // Remover todos os caracteres que não sejam dígitos
-        string digits = new string(cpf.Where(char.IsDigit).ToArray());
-
-        // Verificar se há 11 dígitos
-        if (digits.Length != 11)
-            return false;
-
-        // Verificar se todos os dígitos são iguais
-        if (digits.Distinct().Count() == 1)
-            return false;
-
-        // Calcular o primeiro dígito verificador
-        int sum = 0;
-        for (int i = 0; i < 9; i++)
-        {
-            sum += int.Parse(digits[i].ToString()) * (10 - i);
-        }
-        int firstVerifier = 11 - (sum % 11);
-        if (firstVerifier >= 10)
-            firstVerifier = 0;
-
-        // Calcular o segundo dígito verificador
-        sum = 0;
-        for (int i = 0; i < 10; i++)
-        {
-            sum += int.Parse(digits[i].ToString()) * (11 - i);
-        }
-        int secondVerifier = 11 - (sum % 11);
-        if (secondVerifier >= 10)
-            secondVerifier = 0;
-
-        // Verificar se os dígitos verificadores calculados correspondem aos dígitos informados
-        return digits[9] == firstVerifier.ToString()[0] && digits[10] == secondVerifier.ToString()[0];
     }
 }
